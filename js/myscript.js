a66e32a4c61b1bb5d5eaad75325967616d5eafa8
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-05-31 01:07:13 +0200

    reated BombList

--- js/myscript.js
+++ js/myscript.js
@@ -24,9 +24,13 @@ function startNewGame(){
     const level = parseInt(document.getElementById('level-select').value);
     const outputBanner = document.querySelector('h2.info-banner');
 
-    
+    let userScore = 0;
     let cellsNumber = 0;
     let cellsClass;
+    let isGameOver = false;
+
+
+
     if (level === 0){
         cellsNumber = 100;
         cellsClass = 'cell-easy';
@@ -37,7 +41,21 @@ function startNewGame(){
         cellsNumber = 49;
         cellsClass = 'cell-hard';
     }
+
+
+
+
+    const bombsList = getRandomUniqueNumber(1, cellsNumber, 16);
+    console.log(bombsList);
+
+
+
+
+    outputBanner.innerHTML = "Welcome! Click a cell to get a point!";
     gridElement.innerHTML = "";
+    gridElement.classList.remove('game-over');
+
+
     for (let index = 0; index < cellsNumber; index++) {
         const newCell = createElement('div','cell '+ cellsClass,
                         `<p>${index + 1}</p>`);
